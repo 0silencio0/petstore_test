b3c57470aeeaa2184083e106c80cb26c1de21c0b
silencio <silencio@email.com>
2023-08-19 22:35:03 +0600

    added tested endpoints

---
+++ endpoints.py
@@ -0,0 +1,8 @@
+USER = {
+    'login': '/user/login',
+    'logout': '/user/logout',
+    'create': '/user',
+    'get_by_username': '/user/{username}',
+    'delete': '/user/{username}',
+    'update': '/user/{username}'
+}
